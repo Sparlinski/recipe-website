77d937e27bd72d4010fb98887244593a7fe6b91b
Sparlinski <sparlinski@gmail.com>
2024-08-28 10:50:56 -0500

    Added thai-curry recipe and some minor alterations to files

--- featured-script.js
+++ featured-script.js
@@ -1,67 +1,75 @@
 const featuredRecipes = [
-    {
+    /* {
         url: 'images/bacon-wrapped-meatballs.jpg',
-        link: 'homepage.html',
+        link: 'bacon-wrapped-meatballs.html',
         adjective: 'Baconlicious',
         name: 'BACON WRAPPED MEATBALLS',
         description: "Everything is better wrapped in bacon! This delicious take on meatballs combines the elements of a traditional meatloaf recipe and adds a twist by wrapping each meatball in delicious bacon. It pairs well with the traditional mashed potatoes that meatloaf is known for.",
         alt: 'Bacon Wrapped Meatballs'
-    },
+    }, */
     {
         url: 'images/lemon butter chicken.jpg',
-        link: 'homepage.html',
+        link: 'lemon-chicken.html',
         adjective:  'Lemony Fresh',
         name: 'LEMON BUTTER CHICKEN',
         description: "A traditional take on this classic Italian dish, bursting with flavor and citrus, paired with fresh greens and mashed potatoes. A simple yet full-flavored dish.",
         alt: 'Lemon Butter Chicken'
     },
-    {
+    /* {
         url: 'images/mexican style beef with beans-v2.jpg',
-        link: 'homepage.html',
+        link: 'mexican-beef-beans.html',
         adjective: 'Fiery',
         name: 'MEXICAN STYLE BEEF WITH BEANS',
         description: "Stews have a warm place in everyone's hearts, they are a perfect complete dish that can be very versatile to one's own tastes and preferences. This take on a stew brings the traditional approach with a Mexican twist. A simple and confident mix of flavor and spices.",
         alt: 'Mexican Style Beef Stew'
-    },
-    {
+    }, */
+    /* {
         url: 'images/tika-masala.jpg',
-        link: 'homepage.html',
+        link: 'tika-masala.html',
         adjective: 'Savory',
         name: 'TIKA-MASALA',
         description: "This dish is packed with flavor. The time it is allowed in the slow cooker is what brings this dish together, the flavors and spices are given the time to mix and mingle to really deliver an enjoyable experience, good things come to those who wait after all.",
         alt: 'Tika-Masala'
-    },
-    {
+    }, */
+    /* {
         url: 'images/turkey olive smoked paprika meatballs.jpg',
-        link: 'homepage.html',
+        link: 'turkey-olive-paprika-meatballs.html',
         adjective: 'Smokey',
         name: 'TURKEY OLIVE SMOKED PAPRIKA MEATBALLS',
         description: "This creative combination of flavors really makes for a unique dish. The smokiness of the paprika combines with the saltiness of the olives to complement the acidity of the fire-roasted tomato sauce. Paired with pasta, you have a fine meal that is sure to win anyone over.",
         alt: 'Turkey Olive Smoked Paprika Meatballs'
-    },
+    }, */
     {
         url: 'images/chicken-wings-v2.jpg',
-        link: 'homepage.html',
+        link: 'wings.html',
         adjective: 'Baked',
         name: 'CRISPY BAKED WINGS',
         description: "These wings are crispy, juicy, and full of flavor. Perfectly paired with potato wedges and roasted brussel sprouts in balsamic vinegar with a side of your favorite dipping sauce.",
         alt: 'Crispy Baked Chicken Wings'
     },
-    {
+    /* {
         url: 'images/shredded-chicken-v2.jpg',
-        link: 'homepage.html',
+        link: 'shredded-chicken.html',
         adjective: 'Creamy',
         name: 'CREAMY SHREDDED CHICKEN WITH BACON AND SPINACH',
         description: "The combination of cream and bacon adds a richness that compliments the shredded chicken perfectly in this dish, rounded off by delicious fresh spinach to bring a perfect balance to this dish.",
         alt: 'Creamy Shredded Chicken With Bacon and Spinach'
-    },
+    }, */
     {
         url: 'images/pot roast.jpg',
-        link: 'homepage.html',
+        link: 'pot-roast.html',
         adjective: 'Hearty',
         name: 'POT ROAST',
-        description: "This classic American dish is combined with influence from a traditional Irish stew. This recipe is intended for a slow cooker dump-and-go approach, but it can also be done stew style in one hour if you don't have a chance during your day to start the dish—a fantastic, full-flavored filling meal with minimal fuss or effort.",
+        description: "This classic American dish is influenced by a traditional Irish stew. This recipe is intended for a slow cooker dump-and-go approach, but it can also be done stew style in one hour if you don't have a chance during your day to start the dish—a fantastic, full-flavored filling meal with minimal fuss or effort.",
         alt: 'Pot Roast'
+    },
+    {
+        url: 'images/thai-red-curry-v2.jpg',
+        link: 'thai-curry.html',
+        adjective: 'Fragrant',
+        name: 'THAI RED CURRY',
+        description: "Thai curries are some of my favorites, bringing with them extra dimensions that differ from their Indian counterparts, and this dish is no different. Full of flavor and heat that is sure to please the senses, made from simple and healthy ingredients.",
+        alt: 'Thai Red Curry'
     }
 
 ];
